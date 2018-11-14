de2a153073c93d177fb15639afaaf63562bcedf4
Isaac Smith <isaacns98@gmail.com>
2018-11-14 09:39:15 -0500

    Working dungeon_generation.json

--- src/generator.py
+++ src/generator.py
@@ -53,8 +53,8 @@ class DungeonMap:
         self.min_room_width = config["min_room_width"]
         self.min_room_height = config["min_room_height"]
         self.roomList = []
-        self.actual_height = random.randint(self.min_dun_height, self.max_dun_height + 1)
-        self.actual_width = random.randint(self.min_dun_width, self.max_dun_width + 1)
+        self.actual_height = random.randint(self.min_dun_height, self.max_dun_height)
+        self.actual_width = random.randint(self.min_dun_width, self.max_dun_width)
         self.map = [ [ ' ' for i in range(0, self.actual_width) ] for j in range(0, self.actual_height)]
 
     def createRooms(self):
