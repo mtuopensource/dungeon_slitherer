d5aa2a4ae47ab60109e43f7e8058115c5d93db2a
Isaac Smith <isaacns98@gmail.com>
2018-11-27 19:23:09 -0500

    added source to picture

--- src/models/Player.py
+++ src/models/Player.py
@@ -4,4 +4,5 @@ from pygame import Surface
 
 class Player(Entity):
     def __init__(self):
+		'''https://opengameart.org/content/hero-spritesheets-ars-notoria'''
         Entity.__init__(self, "player.png")
